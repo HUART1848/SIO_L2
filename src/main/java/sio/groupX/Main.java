66af3c9d50e59423f3a71b917dbf2f82fe5764bf
Hugo Huart <hugo.huart@heig-vd.ch>
2023-11-02 13:21:24 +0100

    Initial commit

---
+++ src/main/java/sio/groupX/Main.java
@@ -0,0 +1,23 @@
+package sio.groupX;
+
+public final class Main {
+  public static void main(String[] args) {
+    // TODO
+    //  - Renommage du package ;
+    //  - Implémentation des classes TwoOptBestImprovement et TwoOptFirstImprovement ;
+    //  - Documentation abondante des classes comprenant :
+    //    - la javadoc, avec auteurs et description des implémentations ;
+    //    - des commentaires sur les différentes parties de vos algorithmes.
+
+    // Longueurs optimales :
+    // att532 : 86729
+    // rat575 : 6773
+    // rl1889 : 316536
+    // u574   : 36905
+    // u1817  : 57201
+    // vm1748 : 336556
+
+    // Exemple de lecture d'un jeu de données :
+    // TspData data = TspData.fromFile("data/att532.dat");
+  }
+}
---
+++ src/main/java/sio/groupX/TwoOptBestImprovement.java
@@ -0,0 +1,12 @@
+package sio.groupX;
+
+import sio.tsp.TspImprovementHeuristic;
+import sio.tsp.TspTour;
+
+public final class TwoOptBestImprovement implements TspImprovementHeuristic {
+  @Override
+  public TspTour computeTour(TspTour tspTour) {
+    // TODO
+    return null;
+  }
+}
